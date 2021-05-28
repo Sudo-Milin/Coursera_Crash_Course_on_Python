e771a11414dd7a675c614531ac13fe12ea3e8600
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-05-28 17:09:17 +0530

    Create Graded_Assesment_Q1.py

---
+++ Module_3/Graded_Assesment_Q1.py
@@ -0,0 +1,8 @@
+### Fill in the blanks of this code to print out the numbers 1 through 7.
+
+number = 1
+# Loop continues until the number variable reaches value of 7.
+while number <= 7:
+	print(number, end=" ")
+    # After printing the current number the number variable is increamented by one, it keeps changing during every iteration.
+	number+=1
