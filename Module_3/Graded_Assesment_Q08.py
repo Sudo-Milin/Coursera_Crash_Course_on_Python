9ea5a4cf2b1a2068e76995f16b49ea63f8cb5a54
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-05-31 23:43:10 +0530

    Create Graded_Assesment_Q08.py

---
+++ Module_3/Graded_Assesment_Q08.py
@@ -0,0 +1,6 @@
+What is the value of x at the end of the following code?
+
+for x in range(1, 10, 3):
+    print(x)
+    
+    The upper limit of a range isn’t included, which means that the loop stops before reaching it. The increment is 3, so the loop stops when x reaches 7.
