ae24c406481681bc1ffda6403e519efdd7764318
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-06-02 23:05:14 +0530

    Update Graded_Assesment_Q06.py

--- Module_3/Graded_Assesment_Q06.py
+++ Module_3/Graded_Assesment_Q06.py
@@ -1,12 +1,17 @@
-The even_numbers function returns a space-separated string of all positive numbers that are divisible by 2, up to and including the maximum that's passed into the function. For example, even_numbers(6) returns “2 4 6”. Fill in the blank to make this work.
-
+### The even_numbers function returns a space-separated string of all positive numbers that are divisible by 2, up to and including the maximum that's passed into the function. 
+# For example, even_numbers(6) returns “2 4 6”. Fill in the blank to make this work.
 
 
 def even_numbers(maximum):
+	# Initially an empty variable 'return_string' is created to store the result.
 	return_string = ""
+	# A loop for number starting from 1 to the user given number (+1 because python does not include the last element in range). 
 	for x in range (1,maximum+1):
+		# For every iteration it checks if the value 'x' is divisible by 2 or not (checked by using module operator which returns the value of the remainder in the division operation).
 		if x%2==0:
+			# If it is divisible then it is added to the 'return_string' variable with a space after it
 			return_string += str(x) + " "
+	# At last the the value of 'return_string' is returned along with strip() method which removes white spaces from starting and ending of string
 	return return_string.strip()
 
 print(even_numbers(6))  # Should be 2 4 6
