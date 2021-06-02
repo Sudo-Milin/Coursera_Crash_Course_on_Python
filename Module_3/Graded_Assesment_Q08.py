08db7040b30b0f73a8776154f0097f42b2f532c6
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-06-02 23:20:34 +0530

    Update Graded_Assesment_Q08.py

--- Module_3/Graded_Assesment_Q08.py
+++ Module_3/Graded_Assesment_Q08.py
@@ -1,6 +1,10 @@
-What is the value of x at the end of the following code?
+### What is the value of x at the end of the following code?
 
+# This question in the asssesment is to test your knowledege regarding how much of the interpretation you can do yourself in your mind using the concepts you learnt and remember.
+
+# This is a simple for loop, but the range function takes 3 parameters. 
+# The first one is the starting point of range, the second one is the limit of the range and the last one is the value of increamentation. 
 for x in range(1, 10, 3):
     print(x)
     
-    The upper limit of a range isn’t included, which means that the loop stops before reaching it. The increment is 3, so the loop stops when x reaches 7.
+# The upper limit of a range isn’t included, which means that the loop stops before reaching it. The increment is 3, so the loop stops when x reaches 7 and prints 7
