8e43b4727e51056829987f091b32603beb2ee8c9
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-06-28 12:56:20 +0530

    Update Graded_Assesment_Q07.py

--- Module_4/Graded_Assesment_Q07.py
+++ Module_4/Graded_Assesment_Q07.py
@@ -1,24 +1,33 @@
-Use a dictionary to count the frequency of letters in the input string. Only letters should be counted, not blank spaces, numbers, or punctuation. Upper case should be considered the same as lower case. For example, count_letters("This is a sentence.") should return {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}.
+### Use a dictionary to count the frequency of letters in the input string.
+# Only letters should be counted, not blank spaces, numbers, or punctuation. 
+# Upper case should be considered the same as lower case. 
+# For example, count_letters("This is a sentence.") should return {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}.
 
+# Function defined
 def count_letters(text):
+  # An empty dictionary 'result' to store final result.
   result = {}
-  # Go through each letter in the text
+  # Each letter in text is iterated.
   for letter in text:
-    # Check if the letter needs to be counted or not
+    # Convert each letter to lower case as sugessted.
     letter=letter.lower()
+    # Now check if the letter is an alphabet of not.
     if letter.isalpha()==True:
+      # If yes then check if it is present in result dictionary.
       if letter not in result:
+        # If not present in result dictionary then intialize it in result dictionary.
         result[letter]=0
+      # If already present in result dictionary then increament its value by one.  
       result[letter]+=1
-    # Add or increment the value in the dictionary
-
+  # Finally return the result.
   return result
 
+# Function call
 print(count_letters("AaBbCc"))
 # Should be {'a': 2, 'b': 2, 'c': 2}
-
+# Function call
 print(count_letters("Math is fun! 2+2=4"))
 # Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
-
+# Function call
 print(count_letters("This is a sentence."))
 # Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
