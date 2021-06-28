a96c881019eea08b36cb7ecbed19bc48ceca97b7
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2021-06-28 12:04:51 +0530

    Update Graded_Assesment_Q04.py

--- Module_4/Graded_Assesment_Q04.py
+++ Module_4/Graded_Assesment_Q04.py
@@ -1,9 +1,16 @@
-Use a list comprehension to create a list of squared numbers (n*n). The function receives the variables start and end, and returns a list of squares of consecutive numbers between start and end inclusively.
-For example, squares(2, 3) should return [4, 9].
+### Use a list comprehension to create a list of squared numbers (n*n).
+# The function receives the variables start and end, and returns a list of squares of consecutive numbers between start and end inclusively.
+# For example, squares(2, 3) should return [4, 9].
 
+# Function defined
 def squares(start, end):
+	# This is a one liner, where list comprehension is used instead of iterating over a list by writing a for loop. A list comprehension have a specific format. Here firstly a evaluating equation is given, then a for loop with x variable and its range of iteration. 
 	return [ x*x for x in range (start, end+1) ]
+	# The for loop in list iterates for given range and for every x, it is multiplied by itself giving square of x.
 
+# Function call
 print(squares(2, 3)) # Should be [4, 9]
+# Function call
 print(squares(1, 5)) # Should be [1, 4, 9, 16, 25]
+# Function call
 print(squares(0, 10)) # Should be [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
